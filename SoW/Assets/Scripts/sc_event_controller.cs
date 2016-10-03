7025597320df76a84f7aa85e1b3b9a19242aa614
Roman <nrr_corporation@mail.ru>
2016-10-03 19:56:35 +0200

    continue testing

--- SoW/Assets/Scripts/sc_event_controller.cs
+++ SoW/Assets/Scripts/sc_event_controller.cs
@@ -7,10 +7,12 @@ public class sc_event_controller : MonoBehaviour {
 
     public delegate void void_event_container();
     public delegate void float_event_container(float x);
+    public delegate void intint_event_container(int x, int y);
 
     //public static event float_event_container player_walk;
     public static event void_event_container end_tactik_phase;
     public static event void_event_container end_war_phase;
+    public static event intint_event_container player_tactic_move;
 
     /*void Awake()
     {
@@ -28,5 +30,9 @@ public class sc_event_controller : MonoBehaviour {
     public static void end_war_phase_event()
     {
         end_war_phase();
+    }
+    public static void player_tactic_move_event(int x, int y)
+    {
+        player_tactic_move(x, y);
     }
 }
--- SoW/Assets/Scripts/sc_player.cs
+++ SoW/Assets/Scripts/sc_player.cs
@@ -4,6 +4,11 @@ using System.Collections;
 public class sc_player : MonoBehaviour {
 
     public static sc_player _;
+    public GameObject obj_marker;
+    
+    public sc_field_cell current_cell;
+    enum phase_type { tactic, war };
+    phase_type phase = phase_type.tactic;
 
 
     void Awake()
@@ -11,13 +16,24 @@ public class sc_player : MonoBehaviour {
         _ = this;
     }
 
-	void Start ()
+    void Start()
     {
+        transform.position = current_cell.transform.position;
+        sc_event_controller.player_tactic_move += tactic_move;
+    }
 
-	}
-	
-	void Update ()
+    void tactic_move(int x, int y)
     {
-
-	}
+        Instantiate(obj_marker, transform.position, Quaternion.identity);
+        if (x == 1)
+            current_cell = current_cell.right;
+        if (x == -1)
+            current_cell = current_cell.left;
+        if (y == 1)
+            current_cell = current_cell.up;
+        if (y == -1)
+            current_cell = current_cell.down;
+        transform.position = current_cell.transform.position;
+        //Instantiate(obj_marker, transform.position, Quaternion.identity);
+    }
 }
