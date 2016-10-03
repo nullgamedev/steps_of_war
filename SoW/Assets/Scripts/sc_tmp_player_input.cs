4c0b3fe80ee6a9859a44490991ebf2f19de8547e
Roman <nrr_corporation@mail.ru>
2016-10-03 20:14:37 +0200

    adding meta-files

---
+++ SoW/Assets/Scripts/sc_tmp_player_input.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sc_tmp_player_input : MonoBehaviour {
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+            sc_event_controller.player_tactic_move_event(0, 1);
+        if (Input.GetKeyDown(KeyCode.S))
+            sc_event_controller.player_tactic_move_event(0, -1);
+        if (Input.GetKeyDown(KeyCode.A))
+            sc_event_controller.player_tactic_move_event(-1, 0);
+        if (Input.GetKeyDown(KeyCode.D))
+            sc_event_controller.player_tactic_move_event(1, 0);
+    }
+}
