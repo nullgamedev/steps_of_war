e61329cd303e654b1c96d8d4134bb7c62424a1f1
Dshifu <y.r.laukhin@gmail.com>
2016-10-06 16:47:50 +0300

    add main menu

---
+++ SoW/Assets/Scripts/sc_Main_menu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sc_Main_menu : MonoBehaviour {
+
+	public void StartGame() {
+		Application.LoadLevel("roman_battle_area");
+	
+	}
+	public void ExitGame() {
+		Application.Quit ();
+	
+	}
+	public void Option()  {
+		
+	
+	
+	}
+
+}
