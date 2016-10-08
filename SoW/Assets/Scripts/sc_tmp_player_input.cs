f9de07ee0d6421b08837529c6d98733607ac4a2a
Dshifu <y.r.laukhin@gmail.com>
2016-10-08 19:25:33 +0300

    Delete wrong message for script

--- SoW/Assets/Scripts/sc_tmp_player_input.cs
+++ SoW/Assets/Scripts/sc_tmp_player_input.cs
@@ -9,15 +9,6 @@ public class sc_tmp_player_input : MonoBehaviour {
     public GameObject Move_Buttons;
     bool pause = false;
 
-  /*  void Start ()
-    {
-        Button Aim = Aim_Button.GetComponent<Button>();
-        Aim.onClick.AddListener(Action_aim);
-        Button Fire = Fire_Button.GetComponent<Button>();
-        Fire.onClick.AddListener(Action_aim);
-
-    }
-    */
     public void Action_aim() {
         Move_Buttons.SetActive(false);
         sc_event_controller.player_tactic_shot_event(enemy, weapoon);
@@ -52,16 +43,7 @@ public class sc_tmp_player_input : MonoBehaviour {
     }
     void Update()
     {
-        /*if (Input.GetKeyDown(KeyCode.W))
-            sc_event_controller.player_tactic_move_event(0, 1);
-        if (Input.GetKeyDown(KeyCode.S))
-            sc_event_controller.player_tactic_move_event(0, -1);
-        if (Input.GetKeyDown(KeyCode.A))
-            sc_event_controller.player_tactic_move_event(-1, 0);
-        if (Input.GetKeyDown(KeyCode.D))
-            sc_event_controller.player_tactic_move_event(1, 0);
-        if (Input.GetKeyDown(KeyCode.Space))
-            sc_event_controller.end_tactik_phase_event();*/
+
         if (Input.GetKeyDown(KeyCode.P))
         {
             if (pause)
@@ -70,9 +52,5 @@ public class sc_tmp_player_input : MonoBehaviour {
                 Time.timeScale = 0f;
             pause = !pause;
         }
-        //if (Input.GetKeyDown(KeyCode.F))
-          //  sc_event_controller.player_tactic_shot_event(enemy, weapoon);
-       // if (Input.GetKeyDown(KeyCode.T))
-         //   sc_event_controller.player_tactic_aim_event(enemy, weapoon);
     }
 }
