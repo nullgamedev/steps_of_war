c52bbadd8401325bdefecc304d055e8aefdf8104
Dshifu <y.r.laukhin@gmail.com>
2016-10-31 18:13:44 +0200

    Input

---
+++ SoW/Assets/Scripts/sc_touch_object.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sc_touch_object : MonoBehaviour {
+    public GameObject enemy;
+    public GameObject weapoon;
+	// Use this for initialization
+	void Start () {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+        public void OnMouseDown ()
+        {
+        sc_event_controller.player_tactic_aim_event(enemy, weapoon);
+        }
+}
