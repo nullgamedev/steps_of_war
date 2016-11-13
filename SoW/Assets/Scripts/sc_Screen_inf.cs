4cded2b6d87a964556e71783006455279f2ceb25
Dshifu <y.r.laukhin@gmail.com>
2016-11-13 23:16:23 +0200

    v2v2

---
+++ SoW/Assets/Scripts/sc_Screen_inf.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class sc_Screen_inf : MonoBehaviour {
+
+	public Text Change_hit;
+	public GameObject Heart1;
+	public GameObject Heart2;
+	public GameObject Heart3;
+    public GameObject Roma;
+
+
+	// Use this for initialization
+	void Start () {
+        Roma.SetActive(true);
+	}
+   
+	// Update is called once per frame
+	void Update () {
+		if (sc_player.HpForYurii == 3) 
+		{
+			Heart3.SetActive (false);
+		
+		}
+		if (sc_player.HpForYurii == 2) 
+		{
+			Heart2.SetActive (false);
+		}
+		if (sc_player.HpForYurii == 1) 
+		{
+			Heart1.SetActive (false);
+		}
+	
+		//"change to hit -- " + sc_player.HitChangeForYurii +
+		//Change_hit.text =  "Hp = " + sc_player.HpForYurii;
+	}
+}
---
+++ SoW/Assets/Scripts/sc_first_touch.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class sc_first_touch : MonoBehaviour {
+
+    public GameObject Green_screen;
+    public GameObject Roma;
+    public Text What_Roma_say;
+    public GameObject Canvas1;
+	// Use this for initialization
+	void Start () {
+        Roma.SetActive(true);
+        Green_screen.SetActive(true);
+        Canvas1.SetActive(false);
+        What_Roma_say.text = "Hello, Soldier! Time to roll thats shit! \n  We have time till 9 december. 10 december it's time to push game to Apple Store and Google Play! \n Plz, Guys! Lets do this!";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnMouseDown()
+    {
+        Start_Game();
+    }
+    public void Start_Game()
+    {
+        Green_screen.SetActive(false);
+        Roma.SetActive(false);
+        Canvas1.SetActive(true);
+    }
+}
